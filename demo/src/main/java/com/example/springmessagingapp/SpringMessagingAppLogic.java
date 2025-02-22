f969ed855b61f4f9cb8ac7086aac95545f379757
Deepak <singh017deepak@gmail.com>
2025-02-22 10:47:07 +0530

    [Deepak] Add. Code of Spring Messaging App UC2

--- demo/src/main/java/com/example/springmessagingapp/SpringMessagingAppLogic.java
+++ demo/src/main/java/com/example/springmessagingapp/SpringMessagingAppLogic.java
@@ -1,9 +1,11 @@
 package com.example.springmessagingapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController          //removed controller because it was a subset of restcontroller
+@RequestMapping("/sayhello")
+
 public class SpringMessagingAppLogic {
     @GetMapping("/hello")
     public String hello(){
@@ -11,5 +13,10 @@ public class SpringMessagingAppLogic {
     }
     //UC1 has been completed
 
+    @GetMapping("/query")
+    public String sayhello(@RequestParam(value="name")String name){
+        return "Hello "+name+"! from BridgeLabz...";
+    }
+   //UC2 has been completed
 
 }
