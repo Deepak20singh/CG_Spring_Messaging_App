e0135b30bb797c49cf49d5d254de32289d717a46
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-22 12:49:41 +0530

    [Deepak] Merge: Code of printing hello bridgelabz UC1

---
+++ demo/src/main/java/com/example/springmessagingapp/SpringMessagingAppLogic.java
@@ -0,0 +1,22 @@
+package com.example.springmessagingapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController          //removed controller because it was a subset of restcontroller
+@RequestMapping("/sayhello")
+
+public class SpringMessagingAppLogic {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    //UC1 has been completed
+
+    @GetMapping("/query")
+    public String sayhello(@RequestParam(value="name")String name){
+        return "Hello "+name+"! from BridgeLabz...";
+    }
+   //UC2 has been completed
+
+}
